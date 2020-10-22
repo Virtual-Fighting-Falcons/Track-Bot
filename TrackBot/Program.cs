9598cca9e5ad2aad4df8fed70ee410eb4a9f8491
Sloan Phillippi <sphillippi@Sloans-MBP.lan1>
2020-10-22 18:52:45 -0400

    Added basic bot command handling

--- TrackBot/Program.cs
+++ TrackBot/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+
+public class TrackModule : ModuleBase<SocketCommandContext>
+{
+    [Command("test")]
+    public async Task TestAsync()
+    {
+        await Context.Channel.SendMessageAsync("Test");
+    }
+}
 
 public class Program
 {
-    public static void Main(string[] args)
-    => new Program().MainAsync().GetAwaiter().GetResult();
+    public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
     private DiscordSocketClient _client;
+    private CommandService _commands;
 
     public async Task MainAsync()
     {
@@ -16,9 +27,14 @@ public class Program
         _client.Log += Log;
 
         var token = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "token.txt");
+        _commands = new CommandService();
+        CommandHandler handler = new CommandHandler(_client, _commands);
+        await handler.InstallCommandsAsync();
 
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
+        
+        
 
         await Task.Delay(-1);
     }
@@ -29,3 +45,43 @@ public class Program
         return Task.CompletedTask;
     }
 }
+
+public class CommandHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+
+    public CommandHandler(DiscordSocketClient client, CommandService commands)
+    {
+        _commands = commands;
+        _client = client;
+    }
+
+    public async Task InstallCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+
+        await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        var message = messageParam as SocketUserMessage;
+        if (message == null) return;
+
+        int argPos = 0;
+
+        if (!(message.HasStringPrefix("!trackbot ", ref argPos) ||
+            message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
+            return;
+
+        if (!message.HasStringPrefix("!trackbot ", ref argPos) || !message.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
+        if (message.Author.IsBot) return;
+
+        var context = new SocketCommandContext(_client, message);
+
+        await _commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+    }
+}
+
